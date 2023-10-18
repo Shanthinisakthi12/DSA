bbf9f69eac4aff70b2953cec48120a6015174b4d
Shanthini Sakthivel <112458855+Shanthinisakthi12@users.noreply.github.com>
2023-10-18 12:43:17 +0530

    Create BST.c

---
+++ BST.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define a structure for a binary tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node with the given value
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a value into the binary search tree
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+
+// In-order traversal (LNR)
+void inOrder(struct Node* root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+// Pre-order traversal (NLR)
+void preOrder(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+// Post-order traversal (LRN)
+void postOrder(struct Node* root) {
+    if (root != NULL) {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+
+    // Insert values into the binary search tree
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    // Perform in-order traversal
+    printf("In-order traversal: ");
+    inOrder(root);
+    printf("\n");
+
+    // Perform pre-order traversal
+    printf("Pre-order traversal: ");
+    preOrder(root);
+    printf("\n");
+
+    // Perform post-order traversal
+    printf("Post-order traversal: ");
+    postOrder(root);
+    printf("\n");
+
+    return 0;
+}
