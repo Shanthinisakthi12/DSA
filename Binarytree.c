396b8b7135274be23a7d60156195fcf682a53c2f
Shanthini Sakthivel <112458855+Shanthinisakthi12@users.noreply.github.com>
2023-10-18 09:57:48 +0530

    Create Binarytree.c

---
+++ Binarytree.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure of a binary tree node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new binary tree node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a node into the binary tree
+struct Node* insertNode(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+    
+    return root;
+}
+
+// In-order traversal (Left, Root, Right)
+void inOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
+// Pre-order traversal (Root, Left, Right)
+void preOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+}
+
+// Post-order traversal (Left, Right, Root)
+void postOrderTraversal(struct Node* root) {
+    if (root != NULL) {
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+
+    // Insert nodes into the binary tree
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 70);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+
+    printf("In-order traversal: ");
+    inOrderTraversal(root);
+    printf("\n");
+
+    printf("Pre-order traversal: ");
+    preOrderTraversal(root);
+    printf("\n");
+
+    printf("Post-order traversal: ");
+    postOrderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
